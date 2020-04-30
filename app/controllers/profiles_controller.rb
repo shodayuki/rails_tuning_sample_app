dae846e918354c0fbe5db5aa893ba02e374a1b05
soartec-lab <info@soartec-lab.work>
2020-04-30 23:25:07 +0900

    Tuning ProfilesController#user_safe?

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
    def index
     @user = User.find(1)
-    raise Forbidden un less user_safe?
+    raise Forbidden unless user_safe?
 
     @skill_categories = @user.skills.map(&:skill_category).uniq
     @articles = @user.articles
@@ -10,8 +10,7 @@ class ProfilesController < ApplicationController
   private
 
   def user_safe?
-    @user.user_cautions.all? do |user_caution|
-      Time.zone.now > user_caution.caution_freeze.end_time
-    end
+    @user.user_cautions.joins(:caution_freeze).
+      where("caution_freezes.end_time > ?", Time.zone.now).blank?
   end
 end
